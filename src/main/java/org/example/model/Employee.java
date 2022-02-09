2a08376f4fe27c8362f570fa3da318bc5b8d7c15
Anshika Singh <58014942+AnshiSingh@users.noreply.github.com>
2022-02-09 16:02:15 +0530

    initial commit

---
+++ src/main/java/org/example/model/Employee.java
@@ -0,0 +1,14 @@
+package org.example.model;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Employee {
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
